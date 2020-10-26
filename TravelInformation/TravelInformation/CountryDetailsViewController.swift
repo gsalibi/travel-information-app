11259d63c673fba25e96f4d08734f773cc408783
João Vitor Lopes Capi <joaovitorlopescapi@gmail.com>
2020-10-26 19:55:12 -0300

    added swiftgen via cocoa pods

--- TravelInformation/TravelInformation/CountryDetailsViewController.swift
+++ TravelInformation/TravelInformation/CountryDetailsViewController.swift
@@ -9,37 +9,16 @@ import UIKit
 
 class CountryDetailsViewController: UIViewController{
     
-    @IBOutlet weak var tableView: UITableView!
+    
     var info: [String] = []
     var cellTitles = ["País: ", "Capital: ", "Modeda: ", "Língua: ", "Exigência de entrada: ", "Exigência de saída: ", "Visto de Turismo: ",  "Visto de Negócios: "]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        
         
     }
 
-
-}
-
-extension CountryDetailsViewController: UITableViewDelegate, UITableViewDataSource{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.info.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
-        cell.textLabel?.numberOfLines = 0 // change the line break to WordWrap
-        cell.textLabel?.text = cellTitles[indexPath.row] + info[indexPath.row]
-        
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
 
 }
 
