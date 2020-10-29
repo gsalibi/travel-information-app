5f5912ed549445bc4d99efe29e5a22aa8389478a
ivan sabino <ivanksabino@gmail.com>
2020-10-29 19:06:01 +0000

    news views
    Still need to config News Views to let us reuse the same view for each news

---
+++ TravelInformation/TravelInformation/View/NewsView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsView.swift
+//  TravelInformation
+//
+//  Created by Ivan Sabino on 29/10/20.
+//
+
+import UIKit
+
+class NewsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
