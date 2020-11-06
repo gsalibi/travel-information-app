a7025a1eac659b6e73bb546fad8c3214bfb7784c
ivan sabino <ivanksabino@gmail.com>
2020-11-06 18:48:52 +0000

    Search bar functions working, just need to make the details for its visual

--- TravelInformation/TravelInformation/Controlller/CountriesViewController.swift
+++ TravelInformation/TravelInformation/Controlller/CountriesViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class CountriesViewController: UIViewController {
+class CountriesViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     //reload the tableview every time we change the array
     var countries: [Country] = [] {
@@ -21,12 +22,17 @@ class CountriesViewController: UIViewController {
     }
     
     var selectedCountry: Country?
+    var dismissTapGesture = UITapGestureRecognizer()
+    var searchCountries: ([Country]?, Int) = ([],0)
+    var filteredCountries: [Country]? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Set tableView
+        //Set tableviewDelegate
         tableView.delegate = self
         tableView.dataSource = self
+        
+        searchBarConfiguration()
         
         //Load tableview cell register
         let nib = UINib.init(nibName: "CountryTableViewCell", bundle: nil)
@@ -42,6 +48,34 @@ class CountriesViewController: UIViewController {
         
         //change back button collor of navigation bar
         self.navigationItem.backBarButtonItem?.tintColor = Asset.detail.color
+        
+    }
+    
+    //Configuration search bar and gesture
+    
+    func searchBarConfiguration(){
+        //Search bar
+        self.searchBar.delegate = self
+        if #available(iOS 13.0, *) {
+            self.searchBar.searchTextField.textColor = Asset.tertiary.color
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        dismissTapGesture =  UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard(sender:)))
+        
+        
+    }
+    
+    //Update and animate TableView
+    func updateTableViewAnimated(){
+        UIView.transition(with: tableView, duration: 0.3, options: .transitionCrossDissolve, animations: {self.tableView.reloadData()}, completion: nil)
+    }
+    
+    //MARK: Gestures Handlers
+    @objc func dismissKeyboard(sender: UITapGestureRecognizer?){
+        self.searchBar.endEditing(true)
+        self.tableView.removeGestureRecognizer(self.dismissTapGesture)
         
     }
     
@@ -61,17 +95,28 @@ class CountriesViewController: UIViewController {
     }
 }
 
-    //MARK: Table View Delegate and Data Source
+//MARK: Table View Delegate and Data Source
 extension CountriesViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.countries.count
+        if let filteredCountries = filteredCountries{
+            return filteredCountries.count
+        }else{
+            return self.countries.count
+        }
     }
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath) as? CountryTableViewCell{
-            let cellCountry = self.countries[indexPath.row]
+            var cellCountry : Country
+            
+            if let filteredCountries = filteredCountries{
+                 cellCountry = filteredCountries[indexPath.row]
+            }else{
+                 cellCountry = self.countries[indexPath.row]
+            }
+            
             cell.name.text = cellCountry.name
             let imageName = cellCountry.name.replacingOccurrences(of: " " , with: "").lowercased().folding(options: .diacriticInsensitive, locale: .current)
             cell.flagImage.image = ImageAsset(name: imageName).image
@@ -85,7 +130,11 @@ extension CountriesViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedCountry = countries[indexPath.row]
+        if let filteredCountries = self.filteredCountries{
+            self.selectedCountry = filteredCountries[indexPath.row]
+        }else{
+            self.selectedCountry = countries[indexPath.row]
+        }
         performSegue(withIdentifier: "countrySelected", sender: self)
         
     }
@@ -95,3 +144,66 @@ extension CountriesViewController: UITableViewDelegate, UITableViewDataSource{
     
 }
 
+//MARK: Search Bar Delegates
+extension CountriesViewController: UISearchBarDelegate{
+    func resetSearchBar() {
+        self.filteredCountries = nil
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        //Add gesture to dismiss keyboard clicking outside the keyboard
+        self.tableView.addGestureRecognizer(self.dismissTapGesture)
+        
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        //When ERASE has to re-search for results, so, we have to save the count of the last string writed in the searchText to verify if was erased
+        
+        if searchText.count < self.searchCountries.1 {
+            resetSearchBar()
+        }
+        
+        //Update size of actual search
+        self.searchCountries.1 = searchText.count
+        
+        //Empty case
+        if searchText.isEmpty == true {
+            resetSearchBar()
+        }
+        
+        //Filter case
+        if searchText.isEmpty == false {
+                if let filtered = filterFor(name: searchText, allCountries: self.countries) {
+                    self.filteredCountries = filtered
+                }
+        }
+        
+        updateTableViewAnimated()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.endEditing(true)
+        self.tableView.removeGestureRecognizer(dismissTapGesture)
+    }
+    
+    func filterFor(name: String, allCountries: [Country]) -> [Country]? {
+        //Transform all in uppercased
+        var name = name.uppercased()
+        name = name.folding(options: .diacriticInsensitive, locale: Locale(identifier: "pt_BR"))
+        
+        let filterCountries = allCountries.filter({ (countries) -> Bool in
+            //Filter for name
+            //Compare with uppercased
+            var countryName = countries.name.uppercased()
+            countryName = countryName.folding(options: .diacriticInsensitive, locale: Locale(identifier: "pt_BR"))
+            if countryName.contains(name) {
+                return true
+            }
+            return false
+        })
+        
+        return filterCountries
+    }
+    
+}
